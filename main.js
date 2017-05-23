197f42d40dcfd7f2bdc988fa6fec501076f09178
lsp0 <liamsaliba@hotmail.com>
2017-05-24 01:00:54 +1000

    did quite a bit tonight eh

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-var d = new Date();
-// var d = randomDate();
+var t1 = setInterval(runEverySecond, 1000);
+var t2 = setInterval(runEveryHour, 3600000);
 
+function runEverySecond(){
+	updateTime();
+}
+
+function runEveryHour(){
+	updateDate();
+}
 
 Date.prototype.getWeek = function() {
     var onejan = new Date(this.getFullYear(), 0, 1);
@@ -17,35 +24,50 @@ updateDate();
 updateTime();
 
 function updateDate() {
+	var d = new Date();
 	var days = ["sun", "mon", "tues", "wednes", "thurs", "fri", "satur"]
-	$("#day").html(days[d.getDay()]);
+	var day = d.getDay();
+	var week = d.getWeek();
+	var term = Math.ceil(week/13);
+	var date = d.getDate();
+	var month = d.getMonth();
+	if(month === 0)
+		month = 12;
+	var year = d.getYear()-100;
+
+	$("#day").html(days[day]);
+	// TODO: make this actually function correctly
+	$("#week").html(week);
+	$("#term").html(term);
 
 /*	var months = ["january", "feburary", "march", "april", "may", "june", "july",
 	"august", "september", "october", "november", "december"]
 	$("#date").html(d.getDate());
 	$("#month").html(months[d.getMonth()]);
 */
-	$("#date").html(d.getDate().leadZero());
-	if(d.getMonth()===0)
-		$("#month").html(12);
-	else
-		$("#month").html(d.getMonth().leadZero());
-	$("#year").html(d.getYear()-100);
-	$("#week").html(d.getWeek());
-	$("#term").html(Math.ceil(d.getWeek()/13));
+	$("#date").html(date.leadZero());
+	$("#month").html(month.leadZero());
+	$("#year").html(year.leadZero());
 }
 
 function updateTime() {
+	var d = new Date();
+	//var d = randomDate();
 	var hours = d.getHours();
 	var minutes = d.getMinutes();
-	var meridian = "pm";
+	var seconds = d.getSeconds();
+	var meridian = "am";
 
-	if(hours === 0 || hours === 12){hours = 12;}
-	else if(hours < 12){meridian = "am";}
-	else {hours = hours%12;}
+	if(hours > 11){
+		meridian = "pm";
+		hours = hours%12;
+	}
+	if(hours === 0){hours = 12;}
 
-	$("#time").html(hours + ":" + minutes.leadZero() + meridian);
+	$("#time").html(hours + ":" + minutes.leadZero() + "<small>:" + seconds.leadZero() + "</small><b>" + meridian + "</b>");
 }
+
+
 
 function randomDate(){
    var startDate = new Date(2015,0,1).getTime();
@@ -56,3 +78,25 @@ function randomDate(){
    return new Date(timestamp);
 }
 
+
+
+
+
+// TODO: Migrate to node.js express server,
+// allow this kind of content to be served by the webserver.
+function getQuote(){
+	console.log(importJSON("quotes.txt"));
+}
+
+// read file helper function
+// https://stackoverflow.com/questions/14446447/javascript-read-local-text-file
+function importJSON(filename){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	        return JSON.parse(this.responseText);
+	    }
+	};
+	xmlhttp.open("GET", filename, true);
+	xmlhttp.send();
+}
